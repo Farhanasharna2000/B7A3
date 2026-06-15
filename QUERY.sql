57a736a61472be4fab005568b6dbb03d44577b45
Farhana Sharna <farhanasharna22@gmail.com>
2026-06-15 12:53:28 +0600

    Complete Query 6

--- QUERY.sql
+++ QUERY.sql
@@ -164,3 +164,20 @@ SELECT
 FROM
   Users
   FULL JOIN Bookings ON Users.user_id = Bookings.user_id
+
+  -- ========================================================================================================================
+  -- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+  -- ========================================================================================================================
+SELECT
+  booking_id,
+  match_id,
+  total_cost
+FROM
+  Bookings
+WHERE
+  total_cost > (
+    SELECT
+      AVG(total_cost)
+    FROM
+      Bookings
+  )
